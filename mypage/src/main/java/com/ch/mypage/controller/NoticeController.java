c44919397afb29048b1d49129abace97ba0cc144
admin <admin@DESKTOP-7EPVFAQ>
2020-08-25 22:15:33 -0700

    0826 2:14 (선호) notice 게시판 추가

---
+++ mypage/src/main/java/com/ch/mypage/controller/NoticeController.java
@@ -0,0 +1,12 @@
+package com.ch.mypage.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.ch.mypage.service.NoticeService;
+
+@Controller
+public class NoticeController {
+	@Autowired 
+	private NoticeService ns;
+}
