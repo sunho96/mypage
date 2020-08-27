c2185fa377e5e0b6f9a4c2d7777b61fa6eb56e31
603_1JO\admin <kimpower369@naver.com>
2020-08-27 14:19:39 +0900

    0827 1419 태윤 AccountController

--- mypage/src/main/java/com/ch/mypage/controller/AccountController.java
+++ mypage/src/main/java/com/ch/mypage/controller/AccountController.java
@@ -2,11 +2,25 @@ package com.ch.mypage.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ch.mypage.service.AccountBookService;
 import com.ch.mypage.service.AccountService;
 
 @Controller
 public class AccountController {
 	@Autowired
 	private AccountService as;
+	@Autowired
+	private AccountBookService abs;
+
+	@RequestMapping("/AccountInsertForm")
+	public String AccountInsertForm() {
+		return "Account/AccountInsertForm";
+	}
+
+	@RequestMapping("/AccountList")
+	public String AccountList() {
+		return "Account/AccountList";
+	}
 }
