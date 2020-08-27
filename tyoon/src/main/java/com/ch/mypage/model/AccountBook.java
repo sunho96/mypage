12ce81173c63de3d18eac36df83bdf5c1a71690b
603_1JO\admin <kimpower369@naver.com>
2020-08-27 18:03:02 +0900

    0827 태윤 account

---
+++ tyoon/src/main/java/com/ch/mypage/model/AccountBook.java
@@ -0,0 +1,17 @@
+package com.ch.mypage.model;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class AccountBook {
+	private int abookNum;
+	private String dealType;
+	private int amount;
+	private String accounts;
+	private String category;
+	private Date regDate;
+	private String memo;
+	private int accountNum;
+}
